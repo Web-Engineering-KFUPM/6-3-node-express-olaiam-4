4fe79c4ab781524770b268a75172fcd170459c8e
olaiam <s202283060@kfupm.edu.sa>
2026-04-06 11:38:57 +0300

    todo3

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -10,3 +10,12 @@ const quotes = [
 // TODO 3: Create and export getRandomQuote function
 //Hint: export function getRandomQuote(){}
 
+// Import random.js file
+import { getRandomInt } from "./utils/random.js";
+
+// TODO 3: Create and export getRandomQuote function
+//Hint: export function getRandomQuote(){}
+export function getRandomQuote() {
+  const index = getRandomInt(quotes.length);
+  return quotes[index];
+}
